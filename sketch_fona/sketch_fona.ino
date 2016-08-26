7f0fa968fcf21f6337ec6afabe1d78a636fef8d5
Avriel Harvey <psu.ftbl@gmail.com>
2016-08-26 16:07:34 -0400

    Added some comments

--- sketch_fona/sketch_fona.ino
+++ sketch_fona/sketch_fona.ino
@@ -18,72 +18,69 @@
  ****************************************************/
 
 /*
-THIS CODE IS STILL IN PROGRESS!
+  THIS CODE IS STILL IN PROGRESS!
 
-Open up the serial console on the Arduino at 115200 baud to interact with FONA
+  Open up the serial console on the Arduino at 115200 baud to interact with FONA
 
-Note that if you need to set a GPRS APN, username, and password scroll down to
-the commented section below at the end of the setup() function.
+  Note that if you need to set a GPRS APN, username, and password scroll down to
+  the commented section below at the end of the setup() function.
 */
+
+/** Header files */
 #include "Adafruit_FONA.h"
+/** Software serial for FONA */
+#include <SoftwareSerial.h>
+/** Temperture sensor library */
 #include <OneWire.h>
+/** Local file that has the API key */
 #include "constants.h"
 
-
-
-
+/** Enable the temperature sensor */
 OneWire  ds(2);  // on pin 2 (a 4.7K resistor is necessary)
 
+/** FONA-specific constants */
 #define FONA_RX  9
 #define FONA_TX  8
 #define FONA_RST 4
 #define FONA_RI  7
 
-// this is a large buffer for replies
-char replybuffer[255];
-
-// We default to using software serial. If you want to use hardware serial
-// (because softserial isnt supported) comment out the following three lines 
-// and uncomment the HardwareSerial line
-#include <SoftwareSerial.h>
+/** Get a pointer for the software serial so we can communicate with the FONA */
 SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
 SoftwareSerial *fonaSerial = &fonaSS;
 
-// Hardware serial is also possible!
-//  HardwareSerial *fonaSerial = &Serial1;
-
-// Use this for FONA 800 and 808s
+/** Define the FONA device */
 Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
-// Use this one for FONA 3G
-//Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
 
 uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
 
 uint8_t type;
 
+/** Setup method - runs once */
 void setup() {
 
+  // Blink the LED a couple of times
   pinMode(13, OUTPUT);
-
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
+  digitalWrite(13, HIGH);    // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
-  
 
+  // Start logging to the Serial monitor
   Serial.begin(115200);
   Serial.println(F("FONA basic test"));
   Serial.println(F("Initializing....(May take 3 seconds)"));
 
+  // Start the FONA serial (not sure if needed)
   fonaSerial->begin(4800);
   if (! fona.begin(*fonaSerial)) {
     Serial.println(F("Couldn't find FONA"));
     while (1);
   }
+
+  // Print the FONA device type
   type = fona.type();
   Serial.println(F("FONA is OK"));
   Serial.print(F("Found "));
@@ -100,10 +97,10 @@ void setup() {
       Serial.println(F("FONA 3G (American)")); break;
     case FONA3G_E:
       Serial.println(F("FONA 3G (European)")); break;
-    default: 
+    default:
       Serial.println(F("???")); break;
   }
-  
+
   // Print module IMEI number.
   char imei[15] = {0}; // MUST use a 16 character buffer for IMEI!
   uint8_t imeiLen = fona.getIMEI(imei);
@@ -111,83 +108,75 @@ void setup() {
     Serial.print("Module IMEI: "); Serial.println(imei);
   }
 
-  // Optionally configure a GPRS APN, username, and password.
-  // You might need to do this to access your network's GPRS/data
-  // network.  Contact your provider for the exact APN, username,
-  // and password values.  Username and password are optional and
-  // can be removed, but APN is required.
-  //fona.setGPRSNetworkSettings(F("your APN"), F("your username"), F("your password"));
-   fona.setGPRSNetworkSettings(F("hologram"), F(""), F("")); //this is for hologram in USA
+  // Set the GPRS APN, username, and password. This is required for hologram.io SIMs in the USA.
+  fona.setGPRSNetworkSettings(F("hologram"), F(""), F(""));
 
   // Optionally configure HTTP gets to follow redirects over SSL.
   // Default is not to follow SSL redirects, however if you uncomment
   // the following line then redirects over SSL will be followed.
   //fona.setHTTPSRedirect(true);
 
-    // turn GPRS on
-    boolean gprs = false;
-    while (!gprs) {
-        digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-      gprs = fona.enableGPRS(true);
-      if (!gprs) {
-        Serial.println(F("Failed to turn on"));
-        
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
-      }
-        delay(5000);
+  /** Turn GPRS on for data. If it doesn't work, try again every 5 seconds until it does.
+      Additionally, the LED will stay HIGH once GPRS is turned on. */
+  boolean gprs = false;
+  while (!gprs) {
+    digitalWrite(13, HIGH);    // turn the LED on
+    gprs = fona.enableGPRS(true);
+    if (!gprs) {
+      Serial.println(F("Failed to turn on GPRS"));
+
+      digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+      delay(1000);              // wait for a second
     }
+    delay(5000);
+  }
 
-    
-  digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
-
+  digitalWrite(13, HIGH);    // turn the LED on and hold it
 }
 
-void loop() {  
+void loop() {
   Serial.print(F("FONA> "));
 
-        // read website URL
-        uint16_t statuscode;
-        int16_t length;
-        String key = API_KEY;
-        String urlkey = "api.thingspeak.com/update?api_key=" + key;
-        String url = urlkey + "&field1=" + gettemp();
-        length=url.length();
+  // read website URL
+  uint16_t statuscode;
+  int16_t length;
+  String key = API_KEY;
+  String urlkey = "api.thingspeak.com/update?api_key=" + key;
+  String url = urlkey + "&field1=" + gettemp();
+  length = url.length();
 
-        char charBuf[length];
-        url.toCharArray(charBuf, length);
+  char charBuf[length];
+  url.toCharArray(charBuf, length);
 
-        flushSerial();
-        Serial.println(F("NOTE: in beta! Use small webpages to read!"));
-        Serial.println(F("URL to read (e.g. www.adafruit.com/testwifi/index.html):"));
-        Serial.print(F("http://"));
-        Serial.println(url);
+  flushSerial();
+  Serial.println(F("NOTE: Calling ThingSpeak API"));
+  Serial.print(F("http://"));
+  Serial.println(url);
 
-        Serial.println(F("****"));
-        if (!fona.HTTP_GET_start(charBuf, &statuscode, (uint16_t *)&length)) {
-          Serial.println("Failed!");
-          return;
-        }
-        while (length > 0) {
-          while (fona.available()) {
-            char c = fona.read();
+  Serial.println(F("****"));
+  if (!fona.HTTP_GET_start(charBuf, &statuscode, (uint16_t *)&length)) {
+    Serial.println("Failed!");
+    return;
+  }
+  while (length > 0) {
+    while (fona.available()) {
+      char c = fona.read();
 
-            // Serial.write is too slow, we'll write directly to Serial register!
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+      // Serial.write is too slow, we'll write directly to Serial register!
+      #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
             loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
             UDR0 = c;
-#else
+      #else
             Serial.write(c);
-#endif
-            length--;
-            if (! length) break;
-          }
-        }
-        Serial.println(F("\n****"));
-        fona.HTTP_GET_end();
-      
-    /*****************************************/
+      #endif
+      length--;
+      if (! length) break;
+    }
+  }
+  Serial.println(F("\n****"));
+  fona.HTTP_GET_end();
+
+  /*****************************************/
 
   // flush input
   flushSerial();
@@ -195,6 +184,7 @@ void loop() {
     Serial.write(fona.read());
   }
 
+  // Wait for 30 minutes before looping again
   delay(1800000);
 
 }
@@ -270,23 +260,23 @@ String gettemp() {
   byte data[12];
   byte addr[8];
   float celsius, fahrenheit;
-  
-    ds.reset_search();
-    delay(250);
-    ds.search(addr);
-  
+
+  ds.reset_search();
+  delay(250);
+  ds.search(addr);
+
   Serial.print("ROM =");
-  for( i = 0; i < 8; i++) {
+  for ( i = 0; i < 8; i++) {
     Serial.write(' ');
     Serial.print(addr[i], HEX);
   }
 
   if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
+    Serial.println("CRC is not valid!");
+    return;
   }
   Serial.println();
- 
+
   // the first ROM byte indicates which chip
   switch (addr[0]) {
     case 0x10:
@@ -304,17 +294,17 @@ String gettemp() {
     default:
       Serial.println("Device is not a DS18x20 family device.");
       return;
-  } 
+  }
 
   ds.reset();
   ds.select(addr);
   ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
+
   delay(1000);     // maybe 750ms is enough, maybe not
   // we might do a ds.depower() here, but the reset will take care of it.
-  
+
   present = ds.reset();
-  ds.select(addr);    
+  ds.select(addr);
   ds.write(0xBE);         // Read Scratchpad
 
   Serial.print("  Data = ");
